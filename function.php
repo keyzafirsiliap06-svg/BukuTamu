0b3090e64d5be689d2c9af72dbd52f0d3a3b8959
keyza <keyzafirsiliap06@gmail.com>
2026-09-05 21:38:38 +0700

    menambahkan modal dan fitur tambah data tamu

--- function.php
+++ function.php
@@ -13,3 +13,25 @@ function query($query)
     }
     return $rows;
 }
+
+// function tambah data
+function tambah_tamu($data)
+{
+    global $koneksi;
+
+    $id_tamu = $data['id_tamu'];
+    $nama_tamu = $data['nama_tamu'];
+    $alamat = $data['alamat'];
+    $no_hp = $data['no_hp'];
+    $bertemu = $data['bertemu'];
+    $kepentingan = $data['kepentingan'];
+
+    $query = "INSERT INTO buku_tamu
+              (id_tamu, Tanggal, Nama_Tamu, Alamat, No_HP, Bertemu, Kepentingan)
+              VALUES
+              ('$id_tamu', CURDATE(), '$nama_tamu', '$alamat', '$no_hp', '$bertemu', '$kepentingan')";
+
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
